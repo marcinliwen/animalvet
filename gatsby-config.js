54b9cee43bd24cc2e14c7ba8c6929d582919b455
marcinliwen <liwen.marcin@gmail.com>
2020-08-23 00:17:39 +0200

    header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `AnimalVet `,
+    description: `Gabinet Weterynaryjny - AnimalVet`,
+    author: `MarcinL`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,20 +15,25 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `AnimalVet`,
+        short_name: `AV`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `##CF315E`,
+        theme_color: `##CF315E`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gtech-icon.png`, // This path is relative to the root of the site.
       },
     },
+    //{
+    //  resolve: `gatsby-plugin-google-analytics`,
+    //  options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+     //   trackingId: "UA-91684421-2",
+     // },
+    //},
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,106 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+import PropTypes from "prop-types"
+import React, {useState, useEffect} from "react"
+
+import menu_btn from "./../images/menu.svg"
+import menu_close from "./../images/cancel.svg"
+
+import "./header.css"
+
+const Header = (props) => {
+  const [menu, setMenu] = useState(false);
+  const toggleHamburger =()=>{setMenu(!menu)}
+
+  useEffect(() => {
+    // Zaktualizuj tytuł dokumentu korzystając z interfejsu API przeglądarki
+    if(menu){document.body.classList.add('menu-open')}
+    else{document.body.classList.remove('menu-open')}
+  });
+
+  return(
+  <header>    
+     <div
+      role = "button"
+      tabIndex={0}
+      onKeyDown={toggleHamburger}
+      className={`navbar-burger burger`}
+      data-target="navMenu"
+      onClick={toggleHamburger}
     >
-      <h1 style={{ margin: 0 }}>
+      {menu?
+      <img src={menu_close} width="24px" height="24px" alt="menu"/>:
+      <img src={menu_btn} width="24px" height="24px" alt="menu"/>
+      
+      }
+    </div>
+         
+    <div className="header">
+      <div className="container">
+        <div className="logo-container">
         <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+            to="/"
+          >
+            <div className="logo">
+          <div>Animal</div><div>Vet</div>
+            </div>
+          
+          </Link>
+        </div>
+          
+
+        <nav 
+          className={menu?"-open":""}
+          //onKeyDown={toggleHamburger}
+          //onClick={toggleHamburger}
         >
-          {siteTitle}
+      
+        <Link className="navbar-item" 
+          activeClass="active"
+          to="/uslugi"
+        >
+          Usługi
         </Link>
-      </h1>
+        <Link className="navbar-item" 
+          activeClass="active"
+          to="/zespol"
+          //spy={true}
+          //smooth={true}
+          //offset={-65}
+          //duration={1000}
+          onClick={toggleHamburger}
+        >
+          Zespół
+        </Link>
+        <Link className="navbar-item" 
+          activeClass="active"
+          to="/pupils"
+          //spy={true}
+          //smooth={true}
+          //offset={-65}
+          //duration={1000}
+          onClick={toggleHamburger}
+        >
+          Nasi pacjenci
+        </Link>
+        <Link className="navbar-item" 
+          activeClass="active"
+          to="/kontakt"
+          onClick={toggleHamburger}
+        >
+          Kontakt
+        </Link>
+
+        <div className="contact mobile"><div><a href="tel:+48600700800">600 700 800</a> </div></div>
+        </nav>
+        <div className="nav_back"  onClick={toggleHamburger}></div>
+        <div className="h_banner">
+        <div className="contact"><a href="tel:+48600700800">600 700 800</a></div>
+      </div>
+    </div>
     </div>
   </header>
-)
+)}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
