e19242bf91baeef76c842fa02eacb4f0394c7184
marcinliwen <liwen.marcin@gmail.com>
2020-11-10 13:40:29 +0100

    style

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,8 +50,8 @@ const IndexPage = props => (
             <h2>Popularne usługi</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         </div>
-        <div className="uslugi-content">
-          <div className="uslugi-cart">
+        <div className="uslugi-content row">
+          <div className="uslugi-cart col-4">
             <div className="uslugi-cart-title">
               <div className="uslugi-icon"><img src={surgery} alt="Hirurgia"/></div>
               <h3>Chirurgia</h3>
@@ -61,7 +61,7 @@ const IndexPage = props => (
               <a className="uslugi-more"><span>Dowiedz się więcej </span><ArrowRight /></a>
             </div>
           </div>
-          <div className="uslugi-cart">
+          <div className="uslugi-cart col-4">
             <div className="uslugi-cart-title">
               <div className="uslugi-icon"><img src={rtg} alt="RTG"/></div>
               <h3>Rtg</h3>
@@ -71,7 +71,7 @@ const IndexPage = props => (
               <a className="uslugi-more"><span>Dowiedz się więcej </span><ArrowRight /></a>
             </div>
           </div>
-          <div className="uslugi-cart">
+          <div className="uslugi-cart col-4">
             <div className="uslugi-cart-title">
               <div className="uslugi-icon"><img src={profilactic} alt="profilaktyka"/></div>
               <h3>Profilaktyka</h3>
@@ -81,7 +81,7 @@ const IndexPage = props => (
               <a className="uslugi-more"><span>Dowiedz się więcej </span><ArrowRight /></a>
             </div>
           </div>
-          <div className="uslugi-cart">
+          <div className="uslugi-cart col-4">
             <div className="uslugi-cart-title">
               <div className="uslugi-icon"><img src={vaccine} alt="Szczepienie"/></div>
               <h3>Szczepienie</h3>
@@ -91,7 +91,7 @@ const IndexPage = props => (
               <a className="uslugi-more"><span>Dowiedz się więcej </span><ArrowRight /></a>
             </div>
           </div>
-          <div className="uslugi-cart">
+          <div className="uslugi-cart col-4">
             <div className="uslugi-cart-title">
               <div className="uslugi-icon"><img src={bandage} alt="Ortopedia"/></div>
               <h3>Ortopedia</h3>
@@ -101,7 +101,7 @@ const IndexPage = props => (
               <a className="uslugi-more"><span>Dowiedz się więcej </span><ArrowRight /></a>
             </div>
           </div>
-          <div className="uslugi-cart for-btn">
+          <div className="uslugi-cart for-btn col-4">
           <Link to="/uslugi" >
             <Button color="#fff" background="#ff6290">Poznaj nasze usługi </Button>
           </Link>
@@ -112,37 +112,44 @@ const IndexPage = props => (
     </section>
     <section id="office">
       <div className="container">
-        <div className="office-container">
-          <div className="office-title"><h3>O nas</h3></div>
-          <div className="office-text">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div className="row">
+          <div className="office-container col-8">
+            <div className="office-title"><h3>O nas</h3></div>
+            <div className="office-text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <Officecounter />
           </div>
-          <Officecounter />
-        </div>
-        <div className="office-img">
-        <Img fluid={props.data.imgOffice.childImageSharp.fluid} />
+          <div className="office-img col-4">
+            <Img fluid={props.data.imgOffice.childImageSharp.fluid} />
+          </div>
         </div>
       </div>
     </section>
     <section id="clients">
-      <div className="container row">
+      <div className="container ">
+        <div className="row">
         <div className="col-4 client-titles">
           <h2>Zaufali nam</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           <Button color="#fff" background="#ff6290">Nasi pacjenci</Button>
         </div>
-        <div className="col-8 row clients-item">
-          <div className="col-6 clients-text">
-            <p><q>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</q></p>
-            <p><b>Właścicielka Bengali</b></p>
+        <div className="col-8 clients-item">
+          <div className="row">
+            <div className="col-6 clients-text">
+              <p><q>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</q></p>
+              <p><b>Właścicielka Bengali</b></p>
+            </div>
+            <div className="col-6">
+            <Img fluid={props.data.bengal.childImageSharp.fluid} />
+            </div>
           </div>
-          <div className="col-6">
-          <Img fluid={props.data.bengal.childImageSharp.fluid} />
-    
-          </div>
+          
         </div>
+        </div>
+        
       </div>
     </section>
     <Subscribe  image={props.data.dog.childImageSharp.fluid}/>
