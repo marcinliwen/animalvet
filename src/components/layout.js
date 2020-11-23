8cfb101fe6d3304b47588c6abac60470d30f640a
marcinliwen <liwen.marcin@gmail.com>
2020-11-23 15:14:33 +0100

    messenger style

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,9 +33,10 @@ const Layout = ({ children }) => {
         <MessengerCustomerChat
     pageId="1480527812217145"
     appId="296614704926289"
-    //htmlRef="<REF_STRING>"
-    theme_color="#ff628b"
-    logged_in_greeting="Dzień dobry, w czym możemy Ci pomóc?"
+        page_id="1480527812217145"
+  theme_color="#ff6290"
+  logged_in_greeting="Dzień dobry, jak możemy Ci pomóc?"
+  logged_out_greeting="Dzień dobry, jak możemy Ci pomóc?"
 
   />
         <Footer />
